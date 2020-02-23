17173cf105b8314899173ffea07d2283e4d881c5
99darshan <dsn.adh@gmail.com>
2020-02-23 10:30:36 +0545

    Setup Boggle Context and reducer for app wide state management

---
+++ client/src/state/boggleContext.js
@@ -0,0 +1,20 @@
+
+import React, { createContext,useReducer } from "react";
+import {boggleReducer} from './boggleReducer';
+
+const initialBoggleState={
+    isFetching: false,
+    hasError: false,
+    // TODO: board and validWords will be fetched from server later
+    board:[['a','d','r'],['d','t','c'],['i','j','o']],
+    validWords:["test","dummy","list"],
+}
+
+export const BoggleContext = createContext();
+export default function BoggleProvider(props){
+    // useReducer hooks returns state and dispatch which are passed down as values from Providers
+    let [state, dispatch] = useReducer(boggleReducer, initialBoggleState); 
+    return(<BoggleContext.Provider value={{state, dispatch}}>
+        {props.children}
+    </BoggleContext.Provider>);
+}
---
+++ client/src/state/boggleReducer.js
@@ -0,0 +1,3 @@
+export const boggleReducer = (state, action) => {
+    // TODO: switch action type, based on action type modify the existing state and create a new one
+}
