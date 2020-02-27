817f90e8328d85dcaacf8973173df9b94ff45740
99darshan <dsn.adh@gmail.com>
2020-02-27 20:18:05 +0545

    setup for page routing to different screens using react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import BoggleProvider from './state/boggleContext';
 import Home from './pages/Home';
+import Game from './pages/Game';
+import GameEnd from './pages/GameEnd';
+import * as routes from './constants/routeConstants';
 
 function App() {
   return (
-    <div className="App">
-     <BoggleProvider>
-       <Home/>
-     </BoggleProvider>
-    </div>
+    <BoggleProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path={routes.HOME} component={Home} exact />
+          <Route path={routes.GAME} component={Game} exact />
+          <Route path={routes.GAME_END} component={GameEnd} exact />
+        </Switch>
+      </BrowserRouter>
+    </BoggleProvider>
   );
 }
 
---
+++ client/src/constants/routeConstants.js
@@ -0,0 +1,3 @@
+export const HOME = '/';
+export const GAME = '/game';
+export const GAME_END = '/game/end';
