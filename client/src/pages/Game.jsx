b327d972eeee783bf169229633d3290c73209229
99darshan <dsn.adh@gmail.com>
2020-03-01 00:03:21 +0545

    added styles to boggle board

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -4,6 +4,8 @@ import { BoggleContext } from "../state/boggleContext";
 import { Redirect } from "react-router-dom";
 import * as routes from "../constants/routeConstants";
 import httpService from "../services/httpService";
+import "../styles/boggle.scss";
+
 import {
   FETCH_BOGGLE_ENDPOINT_SUCCESS,
   API_FETCH_START,
@@ -112,109 +114,82 @@ export default function Game() {
 
   return (
     <>
-      <p>
-        has game ended {state.hasGameEnded} and {state.totalTimeInSec}
-      </p>
-      {state.hasGameEnded && <Redirect to={routes.GAME_END} />}
-      {state.isFetching ? (
-        <p>is fetching data...</p>
-      ) : (
-        <React.Fragment>
-          <Timer totalTimeInSec={state.totalTimeInSec} />
-          {/* Board */}
-          <div style={styles.boggleBoardContainer}>
-            <div style={styles.boggleBoard}>
-              {state.boggleBoard.map((row, rowInd) => {
-                return (
-                  <div style={styles.bbRow}>
-                    {row.map((item, colInd) => (
-                      <div
-                        style={styles.bbCell}
-                        onClick={e => onBoggleCellClick(e, rowInd, colInd)}
-                      >
-                        {item}
-                      </div>
-                    ))}
-                  </div>
-                );
-              })}
+      <div className="game-wrapper">
+        {state.hasGameEnded && <Redirect to={routes.GAME_END} />}
+        {state.isFetching ? (
+          <p>is fetching data...</p>
+        ) : (
+          <React.Fragment>
+            <Timer totalTimeInSec={state.totalTimeInSec} />
+            {/* Board */}
+            <div className="boggle-board-wrapper">
+              <div className="boggle-board">
+                {state.boggleBoard.map((row, rowInd) => {
+                  return (
+                    <div className="board-row">
+                      {row.map((item, colInd) => (
+                        <div
+                          className="board-cell"
+                          onClick={e => onBoggleCellClick(e, rowInd, colInd)}
+                        >
+                          {item}
+                        </div>
+                      ))}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-          {/* Input Field */}
-          <div style={styles.bbInputField}>
-            {inputMode === INPUT_MODES.textField && (
-              <input
-                type="text"
-                name="inputWord"
-                onChange={e => setInputWord(e.target.value)}
-                value={inputWord}
-              />
-            )}
+            {/* Input Field */}
+            <div>
+              {inputMode === INPUT_MODES.textField && (
+                <input
+                  type="text"
+                  name="inputWord"
+                  onChange={e => setInputWord(e.target.value)}
+                  value={inputWord}
+                />
+              )}
 
-            <p>Input Word: {inputWord}</p>
-            <button
-              onClick={e => {
-                setInputWord("");
-                setValidAdjacentcells([]);
-              }}
-            >
-              Reset
-            </button>
-            <button onClick={onWordSubmitted}>Submit Word</button>
-            <button
-              onClick={() =>
-                setInputMode(
-                  inputMode === INPUT_MODES.textField
-                    ? INPUT_MODES.boardClick
-                    : INPUT_MODES.textField
-                )
-              }
-            >
-              {inputMode === INPUT_MODES.textField
-                ? "Switch to Board Click Mode"
-                : "Switch to Text Field Mode"}
-            </button>
-            <div>
-              <p>Correct Words List: </p>
-              {correctWords.map(word => (
-                <p>{word}</p>
-              ))}
+              <p>Input Word: {inputWord}</p>
+              <button
+                onClick={e => {
+                  setInputWord("");
+                  setValidAdjacentcells([]);
+                }}
+              >
+                Reset
+              </button>
+              <button onClick={onWordSubmitted}>Submit Word</button>
+              <button
+                onClick={() =>
+                  setInputMode(
+                    inputMode === INPUT_MODES.textField
+                      ? INPUT_MODES.boardClick
+                      : INPUT_MODES.textField
+                  )
+                }
+              >
+                {inputMode === INPUT_MODES.textField
+                  ? "Switch to Board Click Mode"
+                  : "Switch to Text Field Mode"}
+              </button>
+              <div>
+                <p>Correct Words List: </p>
+                {correctWords.map(word => (
+                  <p>{word}</p>
+                ))}
+              </div>
+              <div>
+                <p>Incorrect Words List: </p>
+                {incorrectWords.map(word => (
+                  <p>{word}</p>
+                ))}
+              </div>
             </div>
-            <div>
-              <p>Incorrect Words List: </p>
-              {incorrectWords.map(word => (
-                <p>{word}</p>
-              ))}
-            </div>
-          </div>
-        </React.Fragment>
-      )}
+          </React.Fragment>
+        )}
+      </div>
     </>
   );
 }
-
-const styles = {
-  boggleBoardContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignContent: "center",
-    width: "100vw",
-    background: "green"
-  },
-  boggleBoard: {
-    width: "80vw",
-    background: "red"
-  },
-  bbRow: {
-    display: "flex",
-    justifyContent: "space-evenly"
-  },
-  bbCell: {
-    border: "1px solid white",
-    padding: "8px",
-    margin: "4px"
-  },
-  bbInputField: {
-    marginTop: "20px"
-  }
-};
