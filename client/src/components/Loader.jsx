e25237162c2f01f1e407ea7e816707b5026c45e4
99darshan <dsn.adh@gmail.com>
2020-03-02 14:08:20 +0545

    added new fonts, style enhancements for loaders

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -4,7 +4,7 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 export default function Loader({ cssClass }) {
   return (
     <div className={cssClass}>
-      <CircularProgress />
+      <CircularProgress color="inherit" />
       <p>Hold your horses...</p>
       <p>Ain't nobody got time to wait, but my algorithm is meh so 🤷‍♀️ 🤦‍♀️ </p>
     </div>
